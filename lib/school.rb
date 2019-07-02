35f97b2da20c56f09a773cc4c53c6011618dc5d1
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-07-02 02:05:46 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,20 +12,22 @@ class School
     @roster
   end
   
-  def add_student(student_name, grade)
-    @student_name = student_name
-    @grade = grade 
-    if roster.include?(grade) == false 
-      @roster[grade]= []
+  def add_student(name, grade)
+    @student_name = name
+    @grade = grade
+    if @roster.include?(grade) == false
+      @roster[grade] = []
     end
-      @roster[grade] << name
+    @roster[grade] << name
   end
-  
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |grade, name|
+      @roster[grade] = name.sort
+    end
+  end
 end
-
-
-
-school = School.new("Bayside High School")
-puts school.name
-school.add_student("Zach Morris", 9)
-school.roster
